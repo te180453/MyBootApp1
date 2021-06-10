a5c47982775cbfec68cc951822b6fac9102c4ae2
te180453 <te180453jc21@gmail.com>
2021-06-10 15:09:44 +0900

    spring boot でテストしてみた

--- src/test/java/jp/te4a/spring/boot/myapp1/AppTest.java
+++ src/test/java/jp/te4a/spring/boot/myapp1/AppTest.java
@@ -1,8 +1,8 @@
 package jp.te4a.spring.boot.myapp1;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
